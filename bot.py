671acde0c9338d15cff1bc059127d805bd9d2f7f
krystelgailsmithstage <77292994+krystelgailsmithstage@users.noreply.github.com>
2022-01-14 22:57:48 -0500

    spawn works

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from game_message import Tick, Position, Team, TickMap, TileType
 from game_command import CommandAction, CommandType
 from action_decision import get_next_action
 
-from spawn import get_spawn_borders
+from spawn import get_spawn_borders, get_closest_spawn_positions
 
 import random
 
@@ -27,7 +27,7 @@ class Bot:
             if not unit.hasSpawned:
                 actions.append(
                     CommandAction(
-                        action=CommandType.SPAWN, unitId=unit.id, target=self.get_random_spawn_position(tick.map)
+                        action=CommandType.SPAWN, unitId=unit.id, target=self.get_spawn_position(tick.map, actions)
                     )
                 )
             else:
@@ -39,7 +39,7 @@ class Bot:
         return actions
 
 
-    def get_random_spawn_position(self, tick_map: TickMap) -> Position:
+    def get_spawn_position(self, tick_map: TickMap, actions) -> Position:
         spawns: List[Position] = []
 
         for x in range(tick_map.get_map_size_x()):
@@ -49,6 +49,7 @@ class Bot:
                     spawns.append(position)
 
         spawnsBorders = get_spawn_borders(spawns, tick_map)
+        nextClosestPosition = get_closest_spawn_positions(spawnsBorders, tick_map, actions)[0][0]
 
+        return nextClosestPosition
 
-        return spawnsBorders[random.randint(0, len(spawnsBorders) - 1)]
--- spawn.py
+++ spawn.py
@@ -1,9 +1,7 @@
 from typing import List
-from game_message import Tick, Position, Team, TickMap, TileType
+from game_message import Tick, Position, Team, TickMap, TileType, Diamond
 from game_command import CommandAction, CommandType
-
-import random
-
+from action_decision import get_closest_diamond, get_targeted_diamonds, get_available_diamonds
 
 
 def get_spawn_borders(spawnPositions: List[Position], tick_map: TickMap) -> List[Position]:
@@ -14,6 +12,21 @@ def get_spawn_borders(spawnPositions: List[Position], tick_map: TickMap) -> List
             spawnsBorder.append(position)
 
     return spawnsBorder
+
+
+def get_closest_spawn_positions(spawnBorders: List[Position], tick_map, actions) -> List[Position]:
+    spawn_closest_diamond_list = []
+
+    targeted_diamonds = get_targeted_diamonds(actions, tick_map.diamonds)
+
+    for spawn in spawnBorders:
+        closest_diamond, shortest_distance = get_spawn_diamond(spawn, get_available_diamonds(targeted_diamonds, tick_map))
+        spawn_closest_diamond_list.append((spawn, shortest_distance))
+
+    spawn_closest_diamond_list.sort(key=lambda x:x[1])
+    spawn_closest_diamond_list = get_available_spawn(get_targeted_spawns(actions, spawn_closest_diamond_list), spawn_closest_diamond_list)
+    return spawn_closest_diamond_list
+
 
 
 
@@ -32,3 +45,36 @@ def has_around(tick_map: TickMap, position: Position, tileType: TileType)  -> bo
     if position.y + 1 <= maxIndexY:
         if tick_map.get_tile_type_at(Position(position.x, position.y + 1)) == tileType:
             return True
+
+
+
+def get_spawn_diamond(pos: Position, diamond_list):
+    closest_diamond = None
+    shortest_distance = None
+
+    for diamond in diamond_list:
+        distance_x = abs(diamond.position.x - pos.x)
+        distance_y = abs(diamond.position.y - pos.y)
+        distance = distance_x + distance_y
+        if shortest_distance is None or distance < shortest_distance:
+            shortest_distance = distance
+            closest_diamond = diamond
+
+    return closest_diamond, shortest_distance
+
+
+
+def get_targeted_spawns(actions, spawns):
+    targeted_spawns = []
+    for action in actions:
+        if action.action == CommandType.SPAWN:
+            for spawn in spawns:
+                if action.target == spawn[0]:
+                    targeted_spawns.append(spawn)
+
+    return targeted_spawns
+
+
+def get_available_spawn(targeted_spawns, spawns):
+    filtered_spans = [spawn for spawn in spawns if spawn not in targeted_spawns]
+    return filtered_spans
