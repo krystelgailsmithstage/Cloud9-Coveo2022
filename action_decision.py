a955b6d7e6abd5d86bdd60be0233c35c42548237
Simon Torres <Thorres@users.noreply.github.com>
2022-01-15 03:05:23 -0500

    Get enemy positions

--- action_decision.py
+++ action_decision.py
@@ -19,13 +19,15 @@ def get_next_action(tick: Tick, unit, actions):
         # Else go in attack mode towards closest enemy with a diamond
         else:
             enemy_close, enemy_position = is_enemy_close(unit, tick)
-
+            enemy_in_sight, enemy_grapple_position = is_enemy_lineofsight(unit, tick, my_team)
             # If an enemy is already close, attack it
             if enemy_close:
                 print(unit.hasDiamond, enemy_position)
-                return CommandType.ATTACK, enemy_position
+                if not tick.map.get_tile_type_at(enemy_position) == TileType.SPAWN:
+                    return CommandType.ATTACK, enemy_position
             # TODO: If enemy is in line of sight, grapple
-
+            if enemy_in_sight:
+                return CommandType.VINE, enemy_grapple_position
             # Else move to closest enemy holding diamond
             else:
                 enemy_diamonds = get_enemy_diamonds(tick.map.diamonds, my_team.units)
@@ -35,10 +37,15 @@ def get_next_action(tick: Tick, unit, actions):
                     return CommandType.MOVE, empty_tiles_around[0]
                 else:
                     return CommandType.NONE, None
+    elif unit.hasDiamond:
+        if get_summon_level_for_unit(unit, tick.map) < 5:
+            return CommandType.SUMMON, unit.position
+        else:
+            return CommandType.NONE, None
     elif tick.tick == tick.totalTick - 1 and unit.hasDiamond:
         return get_drop_action(unit, tick)
     else:
-        return CommandType.NONE, unit.position
+        return CommandType.NONE, None
         # targeted_diamonds = get_targeted_diamonds(actions, tick.map.diamonds)
     #
     # enemy_close, enemy_unit = is_enemy_close(unit, tick)
@@ -70,6 +77,14 @@ def get_all_units_positions(tick: Tick):
     all_positions = []
     for team in tick.teams:
         all_positions.extend([unit.position for unit in team.units])
+
+    return all_positions
+
+def get_all_enemies_positions(tick: Tick, my_team):
+    all_positions = []
+    for team in tick.teams:
+        if not team.id == my_team.id:
+            all_positions.extend([unit.position for unit in team.units])
 
     return all_positions
 
@@ -106,6 +121,7 @@ def get_closest_spawn(diamond, spawns):
 
     return closest_spawn, distance
 
+
 def get_diamond_spawn_pairs(diamonds, spawns):
     pair = []
     for diamond in diamonds:
@@ -114,7 +130,6 @@ def get_diamond_spawn_pairs(diamonds, spawns):
     pair.sort(key=lambda x:x[1])
 
     return pair
-
 
 
 def get_drop_action(unit: Unit, tick):
@@ -165,6 +180,59 @@ def should_attack_close_enemy(unit: Unit, enemy_unit: Unit, tick: Tick):
         return True, enemy_unit.position
     else:
         return False, None
+
+
+def is_enemy_lineofsight(unit: Unit, tick: Tick, my_team):
+    enemies_positions = get_all_enemies_positions(tick, my_team)
+
+    pos = unit.position
+    i = 1
+    x_positive = []
+    while tick.map.validate_tile_exists(pos) and \
+            (not tick.map.get_tile_type_at(pos) == TileType.WALL\
+            or not tick.map.get_tile_type_at(pos) == TileType.SPAWN):
+        pos = Position(x= unit.position.x + i, y= unit.position.y)
+        x_positive.append(pos)
+        i += 1
+
+    pos = unit.position
+    i = 1
+    x_negative = []
+    while tick.map.validate_tile_exists(pos) and \
+            (not tick.map.get_tile_type_at(pos) == TileType.WALL\
+            or not tick.map.get_tile_type_at(pos) == TileType.SPAWN):
+        pos = Position(x= unit.position.x - i, y= unit.position.y)
+        x_negative.append(pos)
+        i += 1
+
+    pos = unit.position
+    i = 1
+    y_positive = []
+    while tick.map.validate_tile_exists(pos) and \
+            (not tick.map.get_tile_type_at(pos) == TileType.WALL\
+            or not tick.map.get_tile_type_at(pos) == TileType.SPAWN):
+        pos = Position(x= unit.position.x, y= unit.position.y + i)
+        y_positive.append(pos)
+        i += 1
+
+    pos = unit.position
+    i = 1
+    y_negative = []
+    while tick.map.validate_tile_exists(pos) and \
+            (not tick.map.get_tile_type_at(pos) == TileType.WALL\
+            or not tick.map.get_tile_type_at(pos) == TileType.SPAWN):
+        pos = Position(x= unit.position.x + i, y= unit.position.y - i)
+        y_negative.append(pos)
+        i += 1
+
+    for enemy_position in enemies_positions:
+        if enemy_position in x_positive \
+            or enemy_position in x_negative \
+                or enemy_position in y_positive \
+                    or enemy_position in y_negative:
+            return True, enemy_position
+
+    return False, None
 
 
 # def get_runaway_position(unit_position: Position, enemy_position: Position, tick_map: TickMap):
